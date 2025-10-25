7b218b65fb7bcd3824ad390fd77fa6ab48df852b
DeekshaNaik028 <naikdeeksh24912@gmail.com>
2025-10-25 21:13:30 +0530

    Delete frontend directory

--- frontend/src/utils/validators.js
+++
@@ -1,9 +0,0 @@
-// utils/validators.js
-export const validateEmail = (email) => {
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return re.test(email);
-};
-
-export const validatePassword = (password) => {
-  return password.length >= 8;
-};
